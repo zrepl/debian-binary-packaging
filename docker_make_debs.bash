b09e2f8f0f20219d8e57ab35a6737cdd1f775797
Christian Schwarz <me@cschwarz.com>
2020-04-18 15:11:47 +0200

    GitHub Worflows CI

--- docker_make_debs.bash
+++ docker_make_debs.bash
@@ -6,10 +6,10 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 docker build -t zrepl_debian_pkg .
 
 for arch in amd64 arm64 armhf i386; do
-docker run --rm -it \
+docker run --rm -i \
     -v "$PWD:/src" \
-    -u $(id -u):$(id -g) \
     -w "/src" \
+    -e ZREPL_DEBIAN_BUILD_NOVERIFY_GPG \
     zrepl_debian_pkg bash /src/ondebian_make_debs.bash "$arch"
 done
 
---
+++ download_ci_release.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+commit="$1"
+goversion="$2"
+
+[ -n "$commit" -a -n "$goversion" ] || ( echo "arguments must not be empty: commit=$commit goversion=$goversion"; exit 1 )
+
+rm -rf release
+
+aws="aws --endpoint-url https://minio.cschwarz.com --no-sign-request"
+
+
+path="s3://zrepl-ci-artifacts/$commit/build-$goversion/"
+$aws s3 sync "$path" ./release
