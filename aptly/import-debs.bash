9def09d895a62bdbcc071efe37bacbca7da0e3f0
Christian Schwarz <me@cschwarz.com>
2020-08-28 12:47:37 +0200

    aptly/import-debs.bash: env vars to delete and then recreate the repo

--- aptly/import-debs.bash
+++ aptly/import-debs.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eo
+set -euo
 set -x
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
@@ -8,7 +8,8 @@ shopt -s expand_aliases # required for env.bash
 source env.bash
 
 REPONAME=unstable
-FLAGS="-architectures=amd64,arm64,armhf,i386 -gpg-key E101418FD3D6FBCB9D65A62D708699FC5F2EBF16"
+FLAGS_ARCHITECTURES="-architectures=amd64,arm64,armhf,i386"
+FLAGS="$FLAGS_ARCHITECTURES -gpg-key E101418FD3D6FBCB9D65A62D708699FC5F2EBF16"
 DISTS=()
 DISTS+=("-distribution=focal   $REPONAME :ubuntu")
 DISTS+=("-distribution=eoan    $REPONAME :ubuntu")
@@ -17,15 +18,23 @@ DISTS+=("-distribution=bionic  $REPONAME :ubuntu")
 DISTS+=("-distribution=buster  $REPONAME :debian")
 DISTS+=("-distribution=stretch $REPONAME :debian")
 
-if [ "y$ZREPL_APTLY_INIT_REPOS" != "y" ]; then
+if [ -v ZREPL_APTLY_INIT_REPOS ]; then
     aptly repo create unstable
     for dist in "${DISTS[@]}"; do
         aptly publish repo $FLAGS $dist
     done
 fi
+if [ -v ZREPL_APTLY_DROP_REPOS ]; then
+    for dist in "${DISTS[@]}"; do
+        dist="${dist#-distribution=}"
+        dist="${dist/$REPONAME/}"
+        aptly publish drop $dist
+    done
+    aptly repo drop unstable
+fi
 
 if stat -t ../debs/zrepl_*changes; then
-    aptly repo include -accept-unsigned ../debs/zrepl_*changes
+    aptly repo include $FLAGS_ARCHITECTURES -accept-unsigned ../debs/zrepl_*changes
 fi
 
 for dist in "${DISTS[@]}"; do
